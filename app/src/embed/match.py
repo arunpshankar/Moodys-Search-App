746b3dbe0d550db177bb02ac75f311954fadf87a
arunpshankar <arunprshankar@gmail.com>
2024-04-05 13:03:29 -0400

    wip

--- app/src/embed/match.py
+++ app/src/embed/match.py
@@ -25,7 +25,7 @@ if __name__ == "__main__":
     text_embedder = VertexAIEmbeddings(model_name=config.TEXT_EMBED_MODEL_NAME)
     text_embedder.instance['batch_size'] = 100
 
-    question = "rhb"
+    question = "Nextracker, Inc"
 
     vector_store = FAISS.load_local("./data/faiss_index/", text_embedder, allow_dangerous_deserialization=True)
     retriever = vector_store.as_retriever(search_type='similarity', search_kwargs={'k': 1, 'fetch_k': 5, 'lambda_mult': 0.9})
